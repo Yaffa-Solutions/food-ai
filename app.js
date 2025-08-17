ff19895d218ea20fc2e6ee8b42fc9c1c6511ac6a
Geneen <geneen850@gmail.com>
2025-08-17 15:01:40 +0300

    create signup authentication

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { join } = require('path'); 
+const authRouter = require('./src/routers/auth');
+
 
 
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
+app.use(express.json());
+app.use('/api/auth/', authRouter);
 
 
 module.exports = app;
